62ac80b45e0143dd3580a8b348cff61ed1cb7020
Ahnaf1943120 <ahnafsiddiquee243@gmail.com>
2025-06-20 18:30:40 +0000

    [automatic commit] 2025-06-20 18:30:39 UTC 129.127.202.31

--- part2/app.js
+++ part2/app.js
@@ -56,21 +56,7 @@ app.post('/login', async (req, res) => {
     res.status(500).json({ error: 'Server error' });
   }
 });
-router.get('/mydogs', async (req, res) => {
-  if (!req.session.user) {
-    return res.status(401).json({ error: 'Not logged in' });
-  }
 
-  try {
-    const [rows] = await db.query(
-      'Select dog_id, name FROM Dogs WHERE owner_id = ?',
-      [req.session.user.user_id]
-    );
-    res.json(rows);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch dogs' });
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
