58d5cea1596626f336ba4d4090ba8a2343e67ea7
Ahnaf1943120 <ahnafsiddiquee243@gmail.com>
2025-06-20 18:06:41 +0000

    [automatic commit] 2025-06-20 18:06:40 UTC 129.127.202.31

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -75,9 +75,8 @@ router.get('/mydogs', async (req, res) => {
   try {
     const [rows] = await db.query(
       'Select dog_id, name FROM Dogs WHERE owner_id = ?',
-      [re]
-
-
+      [req.session.user.user_id ]
+    );
     res.json(rows);
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch dogs' });
