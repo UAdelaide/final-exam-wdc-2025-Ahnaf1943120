50dd401d37f5edc1abe8b19e3b3f9be9b2256d70
Ahnaf1943120 <ahnafsiddiquee243@gmail.com>
2025-06-20 13:40:34 +0000

    [automatic commit] 2025-06-20 13:40:34 UTC 129.127.202.31

--- part2/app.js
+++ part2/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
+const PORT = process.env.PORT || 3000;
 require('dotenv').config();
 
 const app = express();
