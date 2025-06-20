e95f176b73a05c96e979aa4b04aa714a18299bc3
Ahnaf1943120 <ahnafsiddiquee243@gmail.com>
2025-06-20 12:49:52 +0000

    [automatic commit] 2025-06-20 12:49:52 UTC 129.127.202.31

--- part2/app.js
+++ part2/app.js
@@ -23,6 +23,6 @@ app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
 // Login route
-app.post('/login', as)
+app.post('/login', async (req))
 // Export the app instead of listening here
 module.exports = app;
