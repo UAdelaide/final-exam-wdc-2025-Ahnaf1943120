d10dac5093804b2f667058af2b5e257ff71a70aa
Ahnaf1943120 <ahnafsiddiquee243@gmail.com>
2025-06-20 13:43:54 +0000

    [automatic commit] 2025-06-20 13:43:53 UTC 129.127.202.31

--- part2/app.js
+++ part2/app.js
@@ -30,7 +30,7 @@ app.post('/login', async ( req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
 
 // Export the app instead of listening here
